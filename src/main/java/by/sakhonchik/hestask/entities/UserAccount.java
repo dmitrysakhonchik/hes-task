e8f1a3281625009d0e8df761a7a4ac6c4ae51d32
diem <dm.sakhonchik@gmail.com>
2020-10-30 09:40:59 +0300

    add lombok

--- src/main/java/by/sakhonchik/hestask/entities/UserAccount.java
+++ src/main/java/by/sakhonchik/hestask/entities/UserAccount.java
@@ -1,66 +1,25 @@
 package by.sakhonchik.hestask.entities;
 
-import javax.persistence.*;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
 @Entity
-@Table(name = "useraccount", schema = "webapp")
+@Table(name = "user_account", schema = "webapp")
 public class UserAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String Username;
     private String Password;
     private String firstName;
     private String lastName;
+    private String status;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date" , updatable=false)
+    private LocalDate createAt;
 
-    public UserAccount() {
-    }
-
-    public UserAccount(Long id, String username, String password, String firstName, String lastName) {
-        this.id = id;
-        Username = username;
-        Password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return Username;
-    }
-
-    public void setUsername(String username) {
-        Username = username;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
